b529289ff5ef45bcd9c45c75c7715a9da7faecea
luketibbott <luke.tibbott@gmail.com>
2018-10-28 12:26:38 -0700

    Train model on whole data set, improve ROC curve plot

--- keplerutils.py
+++ keplerutils.py
@@ -1,6 +1,9 @@
 # This is a utility file for modeling on Exoplanet classification data
 # Project 3, October 2018
 
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+
 
 def encode_response(x):
     if x == 'CONFIRMED':
@@ -10,7 +13,7 @@ def encode_response(x):
 
 def split_and_upsample(df):
     '''
-        Does a train test split on response and features, uses SMOTE to upsample imputed values
+        Splits data in to train and test sets, uses SMOTE to upsample imputed values
         from minority class to achieve 1:1 ratio of classes, returns the upsampled training response
         and features.
         ----Parameters----
@@ -25,10 +28,34 @@ def split_and_upsample(df):
     y = df.iloc[:, 0]
     X = df.iloc[:, 1:]
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state = 234238971, stratify=y)
     
     sm = SMOTE()
     
-    X_train_res, y_train_res = sm.fit_sample(X_train, y_train.ravel())
+    X_train_res, y_train_res = sm.fit_sample(X_train, y_train)
     
     return X_train_res, X_test, y_train_res, y_test
+
+def split_and_upsample_test(df):
+    '''
+        Splits data in to train and test sets, returns unchanged samples. This function is used for comparison against
+        SMOTE upsampled data.
+        ----Parameters----
+        df: Pandas dataframe with response in 0th column, features in rest of columns
+        ----Returns----
+        X_train: upsampled imputed features
+        y_train: upsampled imputed response
+        X_test: unchanged test features
+        y_test: unchanged test response
+    '''
+    
+    y = df.iloc[:, 0]
+    X = df.iloc[:, 1:]
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 23423421, stratify=y)
+    
+    sm = SMOTE()
+    
+    X_train_res, y_train_res = sm.fit_sample(X_train, y_train)
+    
+    return X_train, X_test, y_train, y_test
